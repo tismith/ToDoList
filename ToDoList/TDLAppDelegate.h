d5781d106614161fca56f47d86c7633d6d2a7951
Toby Smith <toby@tismith.id.au>
2013-11-16 12:14:51 +1000

    Initial Commit

---
+++ ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Toby Smith on 16/11/2013.
+//  Copyright (c) 2013 Toby Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Toby Smith on 16/11/2013.
+//  Copyright (c) 2013 Toby Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDLAppDelegate class]));
+    }
+}
